a00400233431a67888c9caff2e105ec4dbea021d
peppe-p <ombrachiara96@gmail.com>
2021-04-26 14:37:32 +0200

    Add num generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,26 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-utenti_registrati = ["paolo@gmail.com", "mario@gmail.com", "dario@gmail.com"];
-var registrato = false;
+registered_users = ["paolo@gmail.com", "mario@gmail.com", "dario@gmail.com"];
+var registered = false;
 var login = prompt("Inserisci la tua email:");
 
-if (utenti_registrati.includes(login)) {
+if (registered_users.includes(login)) {
     alert("Benvenuto, puoi accedere al gioco!")
-    registrato = true;
+    registered = true;
 } else {
     alert("Non sei registrato, mi dispiace!")
 }
 
 //Gioco dei dadi
+if (registered == true) {
+    //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    var num_user = parseInt(prompt("Scegli un numero da 1 a 6"));
+    var num_pc = Math.floor(Math.random() * 6) + 1;
+
+    //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+    if (num_user > num_pc) {
+
+    }
 
 
-//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+}
+
